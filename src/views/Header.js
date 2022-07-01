a54e12fa06bd15c4ddd5c5fb5e6b5902f37e3c9e
Orlian <55744999+Orlian@users.noreply.github.com>
2022-07-01 21:24:49 +0300

    First components and sass

---
+++ src/views/Header.js
@@ -0,0 +1,17 @@
+import logo from '../assets/logo192.png';
+import LogoutBtn from '../components/LogoutBtn';
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <img className="logo" src={logo} alt="logo" />
+            <h1 className="title">{props.title}</h1>
+            <div className="user-info">
+                <h3 className="username">Welcome {props.name}</h3>
+                <LogoutBtn/>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
